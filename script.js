5201b7e72ef2801d3c37dc0901120d10c8066c6a
maruf <marufalaslam@gmail.com>
2020-11-20 21:05:08 +0600

    file commited via githubdesktop

---
+++ script.js
@@ -0,0 +1,18 @@
+$('.slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots:true,
+});
+
+$('.TestimonialSlider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots:true,
+  });
+
+  hljs.initHighlightingOnLoad();
+  new VGNav().toggle();
